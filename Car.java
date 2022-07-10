5c7f72881ac82001c8ef6de12307a0023bbe9de6
RagaviRamachandran <109016636+RagaviRamachandran@users.noreply.github.com>
2022-07-10 12:57:46 +0400

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,20 @@
+package wk1.day1;
+
+public class Car {
+	
+	public static void main(String[] args) {
+		byte wheel=4;
+		short speed= 15488;
+		int insurancenumber=1254876;
+		long registrationnumber= 1578498945L;
+		float bankbalance=1008.86f;
+		double kilometers=2207852.56;
+		char model='M';
+		
+		System.out.println("This is my result" + speed);
+		System.out.println(wheel);
+		System.out.println("Regestration number" + registrationnumber);
+	}
+		
+
+}
---
+++ FireFox.java
@@ -0,0 +1,14 @@
+package wk1.day1;
+
+public class FireFox {
+	//main method
+	public static void main(String[] args) {
+		float version= 100.2f;
+		String name ="firefox";
+		boolean visible=true;
+		int year=1998;
+		char logo='M';
+		System.out.println("This is My First Script");
+	}
+
+}
